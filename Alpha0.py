616322c7f67bf8a13c91321f693a4354439a9fd5
Jacob <jrmadams02@gmail.com>
2022-09-14 20:58:05 -0400

    setup the stubs and flow of the game

--- Alpha0.py
+++ Alpha0.py
@@ -1,3 +1,10 @@
+from os.path import exists
+
+mark = 'O'
+
+# player 1 = x 2 = o
+timelimit = 10  # seconds
+
 localBoards = [[0 for i in range(9)] for i in range(9)]
 globalBoard = [[0 for i in range(3)] for i in range(3)]
 
@@ -6,5 +13,39 @@ f = open("first_four_moves", "r")
 for line in f:
     localBoards[int(int(line[2]) / 3) * 3 + int(int(line[4]) / 3)][int(line[4]) % 3 + int(line[2]) % 3 * 3] = line[0]
     print(line)
+f.close()
 
 print(localBoards)
+
+
+def readMove():
+    f = open('move_file', 'r')
+    empty = not f.read(1)
+    if empty:
+        global mark
+        mark = 'X'
+    else:
+        localBoards[int(int(f[2]) / 3) * 3 + int(int(f[4]) / 3)][int(f[4]) % 3 + int(f[2]) % 3 * 3] = f[0]  # player o
+    move = choseMove()  # player X
+    makeMove(move[0], move[1])
+
+
+def choseMove():
+    return (0,0)
+
+
+def makeMove(i, j):
+    f = open('move_file', 'w')
+    f.write(f'Alpha0 {i} {j}')
+    f.close()
+
+
+def waitForTurn():
+    while not exists('Alpha0.go'):
+        print('waiting for turn')
+    if exists('end_game'):
+        print('game over, did I win?')
+    else:
+        readMove()
+
+# waitForTurn()
