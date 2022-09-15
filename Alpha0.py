aeb7dba885c047b63d45aef4f4b8eff4852d1490
Jacob <jrmadams02@gmail.com>
2022-09-14 20:28:07 -0400

    read directions

--- Alpha0.py
+++ Alpha0.py
@@ -1,10 +1,10 @@
 localBoards = [[0 for i in range(9)] for i in range(9)]
-globalBoard = [[0 for i in range(3) ] for i in range(3)]
+globalBoard = [[0 for i in range(3)] for i in range(3)]
 
-f = open("first_four_moves","r")
+f = open("first_four_moves", "r")
 
 for line in f:
-    localBoards[int(line[2])][int(line[4])] = line[0]
+    localBoards[int(int(line[2]) / 3) * 3 + int(int(line[4]) / 3)][int(line[4]) % 3 + int(line[2]) % 3 * 3] = line[0]
     print(line)
 
 print(localBoards)
