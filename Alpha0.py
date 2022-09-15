a2f4a428cd40c9b1117796887414295eaec0e9b1
Jacob <jrmadams02@gmail.com>
2022-09-14 20:23:28 -0400

    names

--- Alpha0.py
+++ Alpha0.py
@@ -1,10 +1,10 @@
-bigboy = [[0 for i in range(9)] for i in range(9)]
-overall = [[0 for i in range(3) ] for i in range(3)]
+localBoards = [[0 for i in range(9)] for i in range(9)]
+globalBoard = [[0 for i in range(3) ] for i in range(3)]
 
 f = open("first_four_moves","r")
 
 for line in f:
-    bigboy[int(line[2])][int(line[4])] = line[0]
+    localBoards[int(line[2])][int(line[4])] = line[0]
     print(line)
 
-print(bigboy)
+print(localBoards)
