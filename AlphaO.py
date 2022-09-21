5f126ab0ca64ce4e4637683957e67e723088d0c8
mjaxe <makstin33@gmail.com>
2022-09-21 16:02:20 -0400

    added the function that generates the next possible moves that the player can take

--- AlphaO.py
+++ AlphaO.py
@@ -8,15 +8,16 @@ timelimit = 10  # seconds
 localBoards = [[0 for i in range(9)] for i in range(9)]
 globalBoard = [[0 for i in range(3)] for i in range(3)]
 
-f = open("first_four_moves", "r")
 
-for line in f:
-    localBoards[int(int(line[2]) / 3) * 3 + int(int(line[4]) / 3)][int(line[4]) % 3 + int(line[2]) % 3 * 3] = line[0]
-    print(line)
-f.close()
+#this is an example of reading in a game file and printing out the board (9x9)
+# f = open("first_four_moves", "r")
+
+# for line in f:
+#     localBoards[int(int(line[2]) / 3) * 3 + int(int(line[4]) / 3)][int(line[4]) % 3 + int(line[2]) % 3 * 3] = line[0]
+#     print(line)
+# f.close()
 
 print(localBoards)
-
 
 def readMove():
     f = open('move_file', 'r')
@@ -28,6 +29,17 @@ def readMove():
         localBoards[int(int(f[2]) / 3) * 3 + int(int(f[4]) / 3)][int(f[4]) % 3 + int(f[2]) % 3 * 3] = f[0]  # player o
     move = choseMove()  # player X
     makeMove(move[0], move[1])
+
+#add in the board, from read move, then put in what player we are: -1 or 1
+#this will return the next possible moves that our player can make
+def next_possibleMoves(board, player):
+    next_possible_moves = []
+    for i in range(9):
+        new_board = board.copy()
+        if(new_board[i] == 0):
+            new_board[i] = player
+            next_possible_moves.append(new_board)
+    return next_possible_moves
 
 
 def choseMove():
@@ -47,5 +59,7 @@ def waitForTurn():
         print('game over, did I win?')
     else:
         readMove()
+        choseMove()
+        makeMove()
 
 # waitForTurn()
